825bc38e7e54a5a7e366645384d09b5858363caa
rayankermanshahani <rayankermanshahani@protonmail.com>
2024-03-22 16:58:46 -0400

    only post one proposal at a time

--- bot.py
+++ bot.py
@@ -3,12 +3,14 @@ import os
 import tweepy
 
 # twitter api credentials
-from config import consumer_key, consumer_secret, access_token, access_token_secret
+from config import consumer_key, consumer_secret, bearer_token, access_token, access_token_secret, client_id, client_secret
 
 # authenticate twitter api
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+#api = tweepy.API(auth)
+
+client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
 
 # post new proposals to twitter
 def post():
@@ -27,21 +29,29 @@ def post():
     # iterate over the proposals and post new ones
     for proposal in proposals:
         if proposal["notice_id"] not in posted_proposals:
-            message = f"{proposal['title']}\n\n{proposal['notice_id']}\n\nPublished Date: {proposal['date']}"
+            first_message = f"{proposal['title']}\n\n{proposal['notice_id']}\n\nPublished Date: {proposal['date']}"
             second_message = f"Description:\n{proposal['description']}\n\nLink: {proposal['link']}"
 
             try:
                 # post first tweet 
-                tweet1 = api.update_status(message)
+                first_tweet  = client.create_tweet(text=first_message)
+                first_tweet_id = first_tweet.data["id"]
+
                 # post second tweet as a reply to first tweet
-                tweet2 = api.update_status(second_message, in_reply_to_status_id=tweet1.id_str)
+                second_tweet = client.create_tweet(text=second_message, in_reply_to_tweet_id=first_tweet_id)
+
                 # update the set of posted proposals
                 posted_proposals.add(proposal['notice_id'])
+
+                # save the updated set of posted proposals to the json file
+                with open(posted_proposals_file, "w") as f:
+                    json.dump(list(posted_proposals), f)
+                    
                 print(f"Posted new proposal: {proposal['title']}")
+                return
+
             except tweepy.TweepyException as e:
                 print(f"Error posting proposal: {proposal['title']}")
                 print(f"Error: {str(e)}")
+                return 
 
-    # save the updated set of posted proposals to the json file
-    with open(posted_proposals_file, "w") as f:
-        json.dump(list(posted_proposals), f)
